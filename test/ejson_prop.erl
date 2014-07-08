5f5522093b6e9f9b3f0dde35f0d68c044ea59835
jonasrichard <richard.jonas.76@gmail.com>
2014-07-08 08:28:16 +0200

    Add simple record and rule generator

--- test/ejson_prop.erl
+++ test/ejson_prop.erl
@@ -15,6 +15,27 @@ basic() ->
             {1, binary()}
         ]).
 
+descriptor() ->
+    tuple([atom(), integer()]).
+
+record_name() ->
+    ?LET(Chars, list(choose(33, 127)), list_to_atom(Chars)).
+
+value_and_rule() ->
+    ?LET(N, choose(0, 20),
+         ?LET({Name, Fields, Values},
+              {record_name(), vector(N, record_name()), vector(N, integer())},
+              begin
+                  {list_to_tuple([Name | Fields]),
+                   list_to_tuple([Name | Values])}
+              end
+             )).
+
 basic_prop() ->
-    ?FORALL(Value, basic(),
-        ejson_decode:decode(ejson_encode:encode(Value, []), []) =:= Value).
+    ?FORALL(Value, value_and_rule(),
+            begin
+                ?debugVal(Value),
+                true
+            end).
+%%    ?FORALL(Value, basic(),
+%%        ejson_decode:decode(ejson_encode:encode(Value, []), []) =:= Value).
