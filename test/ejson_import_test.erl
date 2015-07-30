793a5f161922a757e9c8f48d4d2b0889176edc81
Richard Jonas <richard.jonas@erlang-solutions.com>
2015-07-30 12:05:50 +0200

    Add json_include support
    Closes #6

---
+++ test/ejson_import_test.erl
@@ -0,0 +1,19 @@
+-module(ejson_import_test).
+
+-compile({parse_transform, ejson_trans}).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-json_include([ejson_trans_test]).
+
+-json({library, {string, name}, {list, books}}).
+
+include_test_() ->
+    A1 = {author, "John", undefined, "Smith"},
+    A2 = {author, "John", "Davison", "Rockefeller"},
+    L = {library, "home",
+         [{book, "How to draw horses", A1, 1987},
+          {book, "History of rats", A2, 1944}]},
+    {ok, E} = to_json(L),
+    {ok, D} = from_json(E),
+    [?_assertEqual(L, D)].
