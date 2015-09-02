324c38931bc11b28945a3093b2de17e9dda25b13
Dmitry Belyaev <be.dmitry@gmail.com>
2015-09-02 12:01:01 +1000

    Allow value to be set undefined if it is explicitly set as default

--- test/ejson_default.erl
+++ test/ejson_default.erl
@@ -7,10 +7,14 @@
 -include_lib("eunit/include/eunit.hrl").
 
 %% Test for default value for address
--json({book, {string, "title"}, {list, "authors"}, {number, "year"}}).
+-json({book, {string, "title"}, {list, "authors", [{type, person}]}, {number, "year"}}).
 %% People can be authors
 -json({person, {string, "name"},
                {string, "address", [{default, "No address"}]}}).
+
+-json({planet,
+       {binary, "name"},
+       {number, "population", [{default, undefined}]}}).
 
 default_test_() ->
     B = {book, "Treasure Island", [{person, "Robert Louis Stevenson"}], 1911},
@@ -18,11 +22,23 @@ default_test_() ->
     {ok, Dec} = from_json(Enc, book),
     ?_assertMatch({book, _, [{person, _, undefined}], 1911}, Dec).
 
+default_real_test_() ->
+    Enc = <<"{\"title\":\"Treasure Island\","
+            " \"authors\":[{\"name\":\"Robert Louis Stevenson\"}],"
+            " \"year\":1911}">>,
+    {ok, Dec} = from_json(Enc, book),
+    ?_assertMatch({book, "Treasure Island", [{person, "Robert Louis Stevenson", "No address"}], 1911},
+                  Dec).
+
 default_field_test_() ->
     B = {book, "Treasure Island", [{person, "Robert Louis Stevenson"}], 1911},
     {ok, J} = ejson:to_jsx_modules(B, [?MODULE]),
     ?debugVal(J),
     [?_assertEqual(<<"Treasure Island">>, json_path(J, "title")),
-     ?_assertEqual(<<"person">>, json_path(J, "authors.1.__rec")),
      ?_assertEqual(<<"Robert Louis Stevenson">>, json_path(J, "authors.1.name")),
      ?_assertEqual(null, json_path(J, "authors.1.address"))].
+
+default_undefined_test_() ->
+    Enc = <<"{\"name\":\"Mars\"}">>,
+    {ok, Dec} = from_json(Enc, planet),
+    ?_assertMatch({planet, <<"Mars">>, undefined}, Dec).
